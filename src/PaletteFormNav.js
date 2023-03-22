85b0996e23a20f09ed3085944bd6dd60a9b35e64
Tarmo Liivamägi <liivamagi.tarmo@gmail.com>
2023-03-22 22:23:24 +0200

    Tiny style tweaks

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -8,10 +8,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/PaletteFormNavStyles';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 
 
 
@@ -62,9 +62,9 @@ class PaletteFormNav extends Component{
               color="inherit"
               aria-label="Open drawer"
               onClick={this.props.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
+              className={classNames(classes.menuButton,{ [classes.hide]:open})}
             >
-              <MenuIcon />
+              <AddToPhotosIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" noWrap>
               Create A Palette
